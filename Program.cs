129feb445f0ea6ca8fc64450129c611030f0c175
devsnicket <grahamdyson@hotmail.com>
2014-09-13 10:30:05 +0100

    Initial version

---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace DevSnicket.ProcessSpawning
+{
+	internal static class Program
+	{
+		internal static void Main(string[] arguments)
+		{
+			if (!TryInvokeFromSeparateProcess(arguments))
+			{
+				Console.WriteLine("Enter a number and press the enter key to calculate primes:");
+
+				Console.WriteLine(
+					String.Join(
+						", ",
+						InvokeInSeparateProcess(
+							CalcuatePrimeNumbers,
+							Int32.Parse(Console.ReadLine()))));
+			}
+		}
+
+		public static IReadOnlyCollection<Int32> CalcuatePrimeNumbers(Int32 max)
+		{
+			return
+				Enumerable.Range(2, max - 1)
+				.Where(i => Enumerable.Range(2, i - 2).All(j => i % j != 0))
+				.ToArray();
+		}
+
+		private static Boolean TryInvokeFromSeparateProcess(
+			IReadOnlyCollection<String> arguments)
+		{
+			if (arguments.Count != 1 || arguments.First() != _processArgument)
+				return false;
+			else
+			{
+				using (Stream input = Console.OpenStandardInput())
+				using (Stream output = Console.OpenStandardOutput())
+				{
+					var methodAndParameter =
+						Deserialise<MethodAndParameter>(input);
+
+					Serialise(
+						output:
+							output,
+						value:
+							methodAndParameter.Method.Invoke(
+								null,
+								new[] { methodAndParameter.Parameter, }));
+				}
+
+				return true;
+			}
+		}
+		private static TResult InvokeInSeparateProcess<TParameter, TResult>(
+			this Func<TParameter, TResult> method,
+			TParameter parameter)
+		{
+			using (Process process = CreateProcess())
+			{
+				process.Start();
+
+				if (Debugger.IsAttached)
+					AttachDebuggerToProcessId(process.Id);
+
+				using (var output = new MemoryStream())
+				{
+					Serialise(
+						process.StandardInput.BaseStream,
+						new MethodAndParameter
+						{
+							Method = method.Method,
+							Parameter = parameter,
+						});
+
+					while (!process.HasExited)
+						process.StandardOutput.BaseStream.CopyTo(output);
+
+					output.Position = 0;
+
+					return Deserialise<TResult>(output);
+				}
+			}
+		}
+
+		private static T Deserialise<T>(Stream stream)
+		{
+			return (T)new BinaryFormatter().Deserialize(stream);
+		}
+
+		private static void Serialise(
+			Stream output,
+			Object value)
+		{
+			new BinaryFormatter().Serialize(output, value);
+		}
+
+		[Serializable]
+		private class MethodAndParameter
+		{
+			public MethodInfo Method { get; set; }
+			public Object Parameter { get; set; }
+		}
+
+		private static Process CreateProcess()
+		{
+			var process = new Process();
+
+			try
+			{
+				process.StartInfo =
+					new ProcessStartInfo(
+						arguments: _processArgument,
+						fileName: Assembly.GetExecutingAssembly().Location)
+					{
+						RedirectStandardInput = true,
+						RedirectStandardOutput = true,
+						UseShellExecute = false,
+					};
+
+				return process;
+			}
+			catch
+			{
+				process.Dispose();
+				throw;
+			}
+		}
+
+		private const String _processArgument = "ProcessGateway";
+
+		private static void AttachDebuggerToProcessId(
+			Int32 id)
+		{
+			Boolean isAttached = false;
+			
+			do
+			{
+				try
+				{
+					((EnvDTE.DTE)System.Runtime.InteropServices.Marshal.GetActiveObject("VisualStudio.DTE.12.0"))
+					.Debugger
+					.LocalProcesses
+					.Cast<EnvDTE.Process>()
+					.Single(visualStudioProcess => visualStudioProcess.ProcessID == id)
+					.Attach();
+
+					isAttached = true;
+				}
+				catch (COMException e)
+				{
+					if (!e.Message.StartsWith("The message filter indicated that the application is busy."))
+						throw;
+				}
+			}
+			while (!isAttached);
+		}
+	}
+}
